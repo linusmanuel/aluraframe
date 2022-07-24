1a2a05a62011524fea5c49eeebb833f18e8f563d
Joao Lino <linusmannuel@gmail.com>
2022-07-24 14:17:20 +0100

    feat(Negociacao): Programacao defensiva

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,6 +1,6 @@
 class Negociacao {
   constructor(data, quantidade, valor) {
-    this._date = data;
+    this._data = new Date(data.getTime());
     this._quantidade = quantidade;
     this._valor = valor;
     Object.freeze(this);
@@ -10,8 +10,8 @@ class Negociacao {
     return this._quantidade * this._valor;
   }
 
-  get date() {
-    return this._date;
+  get data() {
+    return new Date(this._data.getTime());
   }
 
   get quantidade() {
